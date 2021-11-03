026894de06c2367134c390911114790b7c488285
김민성 <kmsz5813@naver.com>
2021-11-03 16:03:17 +0900

    convertView를 temRow에 옮겨 담아서, 비어 있는 상태라면 채워주기

--- app/src/main/java/com/nepplus/myfrindlistviewpractice_20211103/adapters/FreindAdapter.kt
+++ app/src/main/java/com/nepplus/myfrindlistviewpractice_20211103/adapters/FreindAdapter.kt
@@ -1,11 +1,29 @@
 package com.nepplus.myfrindlistviewpractice_20211103.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.nepplus.myfrindlistviewpractice_20211103.R
 import com.nepplus.myfrindlistviewpractice_20211103.datas.FreindData
 
 class FreindAdapter(
     val mContext: Context,
     val resId: Int,
-    val mList: ArrayList<FreindData>) : ArrayAdapter<FreindData>(  ) {
+    val mList: ArrayList<FreindData>) : ArrayAdapter<FreindData>( mContext, resId, mList ) {
+
+        val mInflater =LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var temRow = convertView
+        if (temRow == null){
+            temRow = mInflater.inflate(R.layout.friend_list_item, null)
+
+        }
+
+    }
+
+
 }
