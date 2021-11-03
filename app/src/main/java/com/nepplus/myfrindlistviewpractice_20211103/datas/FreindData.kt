6b1027e3aa70f538cbb6fbd8373e22a299ab8a9f
김민성 <kmsz5813@naver.com>
2021-11-03 15:49:37 +0900

    친구 한명을 표현하는 데이터 표현용 클래스 제작  - 이름/출생년도/성별/주소 저장

---
+++ app/src/main/java/com/nepplus/myfrindlistviewpractice_20211103/datas/FreindData.kt
@@ -0,0 +1,7 @@
+package com.nepplus.myfrindlistviewpractice_20211103.datas
+
+class FreindData(val name: String,
+                 val birthYear: Int,
+                 val address: String,
+                 val isMale: Boolean) {
+}
