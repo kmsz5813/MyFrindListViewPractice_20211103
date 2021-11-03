87674f9e5f607d40487112acdff88a0be2565a57
김민성 <kmsz5813@naver.com>
2021-11-03 18:17:44 +0900

    친구들 상세보기 화면 추가

---
+++ app/src/main/java/com/nepplus/myfrindlistviewpractice_20211103/ViewFriendActivity.kt
@@ -0,0 +1,11 @@
+package com.nepplus.myfrindlistviewpractice_20211103
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewFriendActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_friend)
+    }
+}
